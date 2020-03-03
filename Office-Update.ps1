280bf9c98bb29bed0d9f54eda917132f7f9e8644
Mike Galvin <digressive@outlook.com>
2020-03-03 13:50:13 +0000

    Major update
    -Cleaned up code
    -Config report
    -Improved e-mail outrput
    -Added ASCII

--- Office-Update.ps1
+++ Office-Update.ps1
@@ -1,10 +1,10 @@
 ﻿<#PSScriptInfo
 
-.VERSION 1.1
+.VERSION 2020.03.01
 
 .GUID 72cb5483-744e-4a7d-bcad-e04462ea2c2e
 
-.AUTHOR Mike Galvin Contact: mike@gal.vin twitter.com/mikegalvin_
+.AUTHOR Mike Galvin Contact: mike@gal.vin / twitter.com/mikegalvin_
 
 .COMPANYNAME Mike Galvin
 
@@ -30,64 +30,72 @@
 
 <#
     .SYNOPSIS
-    Checks for updates to Office Click-to-run source files.
-    
-    .DESCRIPTION
-    A simple script to check for updates to Office click-to-run source files. If updates are available they are downloaded.
-    An email notification can be configured to be sent.
+    Office Update Utility - Office Update Manager.
 
-    Please note: to send a log file using ssl and an SMTP password you must generate an encrypted
-    password file. The password file is unique to both the user and machine.
-    
-    The command is as follows:
+    .DESCRIPTION
+    Checks for updates of Office 365/2019.
+
+    To send a log file via e-mail using ssl and an SMTP password you must generate an encrypted password file.
+    The password file is unique to both the user and machine.
+
+    To create the password file run this command as the user and on the machine that will use the file:
 
     $creds = Get-Credential
     $creds.Password | ConvertFrom-SecureString | Set-Content c:\foo\ps-script-pwd.txt
-   
+
     .PARAMETER Office
-    The location of the Office Deployment Tool. Must be a folder.
+    The folder containing the Office Deployment Tool (ODT).
 
     .PARAMETER Config
-    The name of the configuration xml file for the Office Deployment Tool. It must be in the same folder of the Office deployment tool.
-    
+    The name of the configuration xml file for the Office Deployment Tool.
+    It must be located in the same folder as the Office deployment tool.
+
     .PARAMETER Days
-    The number of days that you wish to keep old update files for. If you do not configure this option, no old update files will be removed.
-    
+    The number of days that you wish to keep old update files for.
+    If you do not configure this option, no old update files will be removed.
+
+    .PARAMETER NoBanner
+    Use this option to hide the ASCII art title in the console.
+
     .PARAMETER L
     The path to output the log file to.
-    The file name will be Office-Update.log.
+    The file name will be Office-Update_YYYY-MM-dd_HH-mm-ss.log
+    Do not add a trailing \ backslash.
 
     .PARAMETER Subject
-    The email subject that the email should have. Encapulate with single or double quotes.
+    The subject line for the e-mail log.
+    Encapsulate with single or double quotes.
+    If no subject is specified, the default of "Office Update Utility Log" will be used.
 
     .PARAMETER SendTo
     The e-mail address the log should be sent to.
 
     .PARAMETER From
-    The from address the log should be sent from.
+    The e-mail address the log should be sent from.
 
     .PARAMETER Smtp
     The DNS name or IP address of the SMTP server.
 
     .PARAMETER User
-    The user account to connect to the SMTP server.
+    The user account to authenticate to the SMTP server.
 
     .PARAMETER Pwd
-    The password for the user account.
+    The txt file containing the encrypted password for SMTP authentication.
 
     .PARAMETER UseSsl
-    Connect to the SMTP server using SSL.
+    Configures the utility to connect to the SMTP server using SSL.
 
     .EXAMPLE
-    Office-Update.ps1 -Office C:\officesrc -Config config.xml -Days 60 -L C:\scripts\logs -Subject 'Server: Office Updates' -SendTo me@contoso.com -From Office-Update@contoso.com -Smtp exch01.contoso.com -User me@contoso.com -Pwd P@ssw0rd -UseSsl
+    Office-Update.ps1 -Office \\Apps01\Software\Office365 -Config config-365-x64.xml -Days 30 -L C:\scripts\logs
+    -Subject 'Server: Office Update' -SendTo me@contoso.com -From OffUpdate@contoso.com -Smtp exch01.contoso.com
+    -User me@contoso.com -Pwd P@ssw0rd -UseSsl
 
-    The above command will run the script, download the Office files to C:\officesrc\Office.
-    It will use a configuration file called config.xml in the C:\officesrc folder.
-    Any update files older than 60 days will be removed.
-    If the download is successful a log file is generated and it can be e-mailed with a custom subject line as a notification that a download occurred.
+    The above command will download any Office updates for the version and channel configured in config-365-x64.xml
+    to the Office files directory \\Apps01\Software\Office365. Any update files older than 30 days will be removed.
+    If the download is successful the log file will be output to C:\scripts\logs and e-mailed with a custom subject line.
 #>
 
-## Set up command line switches and what variables they map to.
+## Set up command line switches.
 [CmdletBinding()]
 Param(
     [parameter(Mandatory=$True)]
@@ -115,7 +123,175 @@ Param(
     [alias("Pwd")]
     [ValidateScript({Test-Path -Path $_ -PathType Leaf})]
     $SmtpPwd,
-    [switch]$UseSsl)
+    [switch]$UseSsl,
+    [switch]$NoBanner)
+
+If ($NoBanner -eq $False)
+{
+    Write-Host -Object ""
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "                                                                "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "     ___  __  __ _                            _       _         "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "    /___\/ _|/ _(_) ___ ___   /\ /\ _ __   __| | __ _| |_ ___   "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "   //  // |_| |_| |/ __/ _ \ / / \ \ '_ \ / _  |/ _  | __/ _ \  "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "  / \_//|  _|  _| | (_|  __/ \ \_/ / |_) | (_| | (_| | ||  __/  "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "  \___/ |_| |_| |_|\___\___|  \___/| .__/ \__,_|\__,_|\__\___|  "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "                                   |_|                          "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "         _   _ _ _ _                                            "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "   /\ /\| |_(_) (_) |_ _   _                                    "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "  / / \ \ __| | | | __| | | |           3 6 5                   "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "  \ \_/ / |_| | | | |_| |_| |          2 0 1 9                  "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "   \___/ \__|_|_|_|\__|\__, |        Click-to-Run               "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "                       |___/                                    "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "                                                                "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "    Mike Galvin    https://gal.vin    Version 2020.03.01        "
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "                                                                "
+    Write-Host -Object ""
+}
+
+## If logging is configured, start logging.
+## If the log file already exists, clear it.
+If ($LogPath)
+{
+    $LogFile = ("Office-Update_{0:yyyy-MM-dd_HH-mm-ss}.log" -f (Get-Date))
+    $Log = "$LogPath\$LogFile"
+
+    $LogT = Test-Path -Path $Log
+
+    If ($LogT)
+    {
+        Clear-Content -Path $Log
+    }
+
+    Add-Content -Path $Log -Encoding ASCII -Value "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") [INFO] Log started"
+}
+
+## Function to get date in specific format.
+Function Get-DateFormat
+{
+    Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+}
+
+## Function for logging.
+Function Write-Log($Type, $Event)
+{
+    If ($Type -eq "Info")
+    {
+        If ($Null -ne $LogPath)
+        {
+            Add-Content -Path $Log -Encoding ASCII -Value "$(Get-DateFormat) [INFO] $Event"
+        }
+        
+        Write-Host -Object "$(Get-DateFormat) [INFO] $Event"
+    }
+
+    If ($Type -eq "Succ")
+    {
+        If ($Null -ne $LogPath)
+        {
+            Add-Content -Path $Log -Encoding ASCII -Value "$(Get-DateFormat) [SUCCESS] $Event"
+        }
+
+        Write-Host -ForegroundColor Green -Object "$(Get-DateFormat) [SUCCESS] $Event"
+    }
+
+    If ($Type -eq "Err")
+    {
+        If ($Null -ne $LogPath)
+        {
+            Add-Content -Path $Log -Encoding ASCII -Value "$(Get-DateFormat) [ERROR] $Event"
+        }
+
+        Write-Host -ForegroundColor Red -BackgroundColor Black -Object "$(Get-DateFormat) [ERROR] $Event"
+    }
+
+    If ($Type -eq "Conf")
+    {
+        If ($Null -ne $LogPath)
+        {
+            Add-Content -Path $Log -Encoding ASCII -Value "$Event"
+        }
+
+        Write-Host -ForegroundColor Cyan -Object "$Event"
+    }
+}
+
+##
+## Display the current config and log if configured.
+##
+Write-Log -Type Conf -Event "************ Running with the following config *************."
+Write-Log -Type Conf -Event "Office folder:.........$OfficeSrc."
+Write-Log -Type Conf -Event "Config file:...........$Cfg."
+Write-Log -Type Conf -Event "Days to keep updates:..$Time days."
+
+If ($Null -ne $LogPath)
+{
+    Write-Log -Type Conf -Event "Logs directory:........$LogPath."
+}
+
+else {
+    Write-Log -Type Conf -Event "Logs directory:........No Config"
+}
+
+If ($MailTo)
+{
+    Write-Log -Type Conf -Event "E-mail log to:.........$MailTo."
+}
+
+else {
+    Write-Log -Type Conf -Event "E-mail log to:.........No Config"
+}
+
+If ($MailFrom)
+{
+    Write-Log -Type Conf -Event "E-mail log from:.......$MailFrom."
+}
+
+else {
+    Write-Log -Type Conf -Event "E-mail log from:.......No Config"
+}
+
+If ($MailSubject)
+{
+    Write-Log -Type Conf -Event "E-mail subject:........$MailSubject."
+}
+
+else {
+    Write-Log -Type Conf -Event "E-mail subject:........Default"
+}
+
+If ($SmtpServer)
+{
+    Write-Log -Type Conf -Event "SMTP server is:........$SmtpServer."
+}
+
+else {
+    Write-Log -Type Conf -Event "SMTP server is:........No Config"
+}
+
+If ($SmtpUser)
+{
+    Write-Log -Type Conf -Event "SMTP user is:..........$SmtpUser."
+}
+
+else {
+    Write-Log -Type Conf -Event "SMTP user is:..........No Config"
+}
+
+If ($SmtpPwd)
+{
+    Write-Log -Type Conf -Event "SMTP pwd file:.........$SmtpPwd."
+}
+
+else {
+    Write-Log -Type Conf -Event "SMTP pwd file:.........No Config"
+}
+
+Write-Log -Type Conf -Event "-UseSSL switch is:.....$UseSsl."
+Write-Log -Type Conf -Event "************************************************************"
+Write-Log -Type Info -Event "Process started"
+##
+## Display current config ends here.
+##
 
 #Run update process.
 & $OfficeSrc\setup.exe /download $OfficeSrc\$Cfg
@@ -126,107 +302,75 @@ $UpdateFolder = "$OfficeSrc\Office\Data"
 ## Check the last write time of the office source files folder if it is greater than the previous day.
 $Updated = (Get-ChildItem -Path $UpdateFolder | Where-Object CreationTime -gt (Get-Date).AddDays(-1)).Count
 
-## If the Updated variable returns as not 0...
+## If the Updated variable returns as not 0 then continue.
 If ($Updated -ne 0)
 {
-    ## If logging is configured, start logging.
-    If ($LogPath)
-    {
-        $LogFile = "Office-Update.log"
-        $Log = "$LogPath\$LogFile"
-
-        ##Test for the existence of the log file.
-        $LogT = Test-Path -Path $Log
-
-        ## If the log file already exists, clear it.
-        If ($LogT)
-        {
-            Clear-Content -Path $Log
-        }
-
-        Add-Content -Path $Log -Value "****************************************"
-        Add-Content -Path $Log -Value "$(Get-Date -Format G) Log started"
-        Add-Content -Path $Log -Value " "
-    }
-
-    Write-Host
-    Write-Host -Object "Office source files were updated."
-    Write-Host -Object "New version is:"
-
-    ## List the update folder contents and the last write time.
-    Get-ChildItem -Path $UpdateFolder -Directory | Where-Object CreationTime –gt (Get-Date).AddDays(-1) | Select-Object -ExpandProperty Name
-
-    ## If logging was configured, write to the log.
-    If ($LogPath)
-    {
-        Add-Content -Path $Log -Value "Office source files were updated"
-        Add-Content -Path $Log -Value "New version is:"
-        Get-ChildItem -Path $UpdateFolder | Where-Object {$_.CreationTime -gt (Get-Date).AddDays(-1)} | Select-Object -Property Name, CreationTime | Out-File -Append $Log -Encoding ASCII
-        Add-Content -Path $Log -Value " "
-    }
+    #$VerName = Get-ChildItem -Path $UpdateFolder -Directory | Where-Object CreationTime –gt (Get-Date).AddDays(-1) | Select-Object -ExpandProperty Name
+    $VerName = Get-ChildItem -Path $UpdateFolder -Directory | Sort-Object LastWriteTime | Select-Object -last 1 | Select-Object -ExpandProperty Name
+    Write-Log -Type Info -Event "Office source files were updated."
+    Write-Log -Type Info -Event "Latest version is: $VerName"
 
     If ($Null -ne $Time)
     {
-        ## If logging was configured, write to the log.
-        If ($LogPath)
+        $FilesToDel = Get-ChildItem -Path $UpdateFolder | Where-Object LastWriteTime –lt (Get-Date).AddDays(-$Time)
+
+        If ($FilesToDel.count -ne 0)
         {
-            Add-Content -Path $Log -Value "Old Office source files were removed:"
-            Get-ChildItem -Path $UpdateFolder | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-$Time)} | Select-Object -Property Name, LastWriteTime | Out-File -Append $Log -Encoding ASCII
-            Add-Content -Path $Log -Value " "
+            Write-Log -Type Info -Event "The following old Office files were removed:"
+            Get-ChildItem -Path $UpdateFolder | Where-Object LastWriteTime –lt (Get-Date).AddDays(-$Time)
+            Get-ChildItem -Path $UpdateFolder | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-$Time)} | Select-Object -Property Name, LastWriteTime | Format-Table -HideTableHeaders | Out-File -Append $Log -Encoding ASCII
+
+            ## If configured, remove the old files.
+            Get-ChildItem $UpdateFolder | Where-Object {$_.LastWriteTime –lt (Get-Date).AddDays(-$Time)} | Remove-Item -Recurse
         }
-
-        Write-Host
-        Write-Host "Old Office source files were removed:"
-        Get-ChildItem -Path $UpdateFolder | Where-Object LastWriteTime –lt (Get-Date).AddDays(-$Time)
-
-        ## If configured, remove the old files.
-        Get-ChildItem $UpdateFolder | Where-Object {$_.LastWriteTime –lt (Get-Date).AddDays(-$Time)} | Remove-Item -Recurse
     }
 
-    ## If logging was configured stop the log.
+    Write-Log -Type Info -Event "Process finished"
+
+    ## If logging is configured then finish the log file.
     If ($LogPath)
     {
-        Add-Content -Path $Log -Value " "
-        Add-Content -Path $Log -Value "$(Get-Date -Format G) Log finished"
-        Add-Content -Path $Log -Value "****************************************"
+        Add-Content -Path $Log -Encoding ASCII -Value "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") [INFO] Log finished"
 
-        ## If email was configured, set the variables for the email subject and body.
+        ## This whole block is for e-mail, if it is configured.
         If ($SmtpServer)
         {
-            # If no subject is set, use the string below.
+            ## Default e-mail subject if none is configured.
             If ($Null -eq $MailSubject)
             {
-                $MailSubject = "Office Update"
+                $MailSubject = "Office Update Utility Log"
             }
 
+            ## Setting the contents of the log to be the e-mail body. 
             $MailBody = Get-Content -Path $Log | Out-String
 
-            ## If an email password was configured, create a variable with the username and password.
+            ## If an smtp password is configured, get the username and password together for authentication.
+            ## If an smtp password is not provided then send the e-mail without authentication and obviously no SSL.
             If ($SmtpPwd)
             {
                 $SmtpPwdEncrypt = Get-Content $SmtpPwd | ConvertTo-SecureString
                 $SmtpCreds = New-Object System.Management.Automation.PSCredential -ArgumentList ($SmtpUser, $SmtpPwdEncrypt)
 
-                ## If ssl was configured, send the email with ssl.
+                ## If -ssl switch is used, send the email with SSL.
+                ## If it isn't then don't use SSL, but still authenticate with the credentials.
                 If ($UseSsl)
                 {
                     Send-MailMessage -To $MailTo -From $MailFrom -Subject $MailSubject -Body $MailBody -SmtpServer $SmtpServer -UseSsl -Credential $SmtpCreds
                 }
 
-                ## If ssl wasn't configured, send the email without ssl.
-                Else
-                {
+                else {
                     Send-MailMessage -To $MailTo -From $MailFrom -Subject $MailSubject -Body $MailBody -SmtpServer $SmtpServer -Credential $SmtpCreds
                 }
             }
 
-            ## If an email username and password were not configured, send the email without authentication.
-            Else
-            {
+            else {
                 Send-MailMessage -To $MailTo -From $MailFrom -Subject $MailSubject -Body $MailBody -SmtpServer $SmtpServer
             }
         }
     }
 }
+
+Write-Log -Type Info -Event "No updates."
+Write-Log -Type Info -Event "Process finished"
 
 ## End
--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-# Automated-Office-Updates
+# Office Update Utility
 
-PowerShell script to automate the downloading and maintenance of click-to-run Office 365/2019 update files.
+Office Update Manager
 
-For full instructions and documentation, [visit my blog post](https://gal.vin/2019/06/16/automated-office-updates/)
+```txt
+     ___  __  __ _                            _       _
+    /___\/ _|/ _(_) ___ ___   /\ /\ _ __   __| | __ _| |_ ___
+   //  // |_| |_| |/ __/ _ \ / / \ \ '_ \ / _  |/ _  | __/ _ \  
+  / \_//|  _|  _| | (_|  __/ \ \_/ / |_) | (_| | (_| | ||  __/  
+  \___/ |_| |_| |_|\___\___|  \___/| .__/ \__,_|\__,_|\__\___|  
+                                   |_|
+         _   _ _ _ _
+   /\ /\| |_(_) (_) |_ _   _
+  / / \ \ __| | | | __| | | |           3 6 5
+  \ \_/ / |_| | | | |_| |_| |          2 0 1 9
+   \___/ \__|_|_|_|\__|\__, |        Click-to-Run
+                       |___/
+
+    Mike Galvin    https://gal.vin    Version 2020.03.01
+```
+
+For full instructions and documentation, [visit my site.](https://gal.vin/2019/06/16/automated-office-updates/)
 
 Please consider donating to support my work:
 
-* You can support me on a monthly basis [using Patreon.](https://www.patreon.com/mikegalvin)
-* You can support me with a one-time payment [using PayPal](https://www.paypal.me/digressive) or by [using Kofi.](https://ko-fi.com/mikegalvin)
+* Sign up [using Patreon.](https://www.patreon.com/mikegalvin)
+* Support with a one-time payment [using PayPal.](https://www.paypal.me/digressive)
 
-Automated Office Updates can also be downloaded from:
+Office Update Utility can also be downloaded from:
 
-* [The Microsoft TechNet Gallery](https://gallery.technet.microsoft.com/Automated-Office-Updates-4fef21d3?redir=0)
-* [The PowerShell Gallery](https://www.powershellgallery.com/packages/Office-Update/1.0)
+* [The Microsoft PowerShell Gallery](https://www.powershellgallery.com/packages/Office-Update)
 
 Tweet me if you have questions: [@mikegalvin_](https://twitter.com/mikegalvin_)
 
@@ -20,14 +36,14 @@ Tweet me if you have questions: [@mikegalvin_](https://twitter.com/mikegalvin_)
 
 ## Features and Requirements
 
-* This script will check for and download update files for click-to-run Office installs such as Office 365 and Office 2019.
-* The script can also remove old update files to prevent bloat.
-* The script can optionally create a log file and e-mail the log file to an address of your choice.
-* The script has been tested on Windows Server 2016 and Windows Server 2019, updating Office 2019 volume licensed installations on Windows 10 1809 and 1903.
+* This utility will check for and download update files for Office 365 and Office 2019.
+* It can be configured to remove old update files.
+* It can be configured to create and e-mail a log file.
+* This utility has been tested on Windows 10, Windows Server 2016 and Windows Server 2019 and updating Office 2019 and 365 installations.
 
 ### Generating A Password File
 
-The password used for SMTP server authentication must be in an encrypted text file. To generate the password file, run the following command in PowerShell, on the computer that is going to run the script and logged in with the user that will be running the script. When you run the command you will be prompted for a username and password. Enter the username and password you want to use to authenticate to your SMTP server.
+The password used for SMTP server authentication must be in an encrypted text file. To generate the password file, run the following command in PowerShell on the computer and logged in with the user that will be running the utility. When you run the command, you will be prompted for a username and password. Enter the username and password you want to use to authenticate to your SMTP server.
 
 Please note: This is only required if you need to authenticate to the SMTP server when send the log via e-mail.
 
@@ -42,76 +58,25 @@ After running the commands, you will have a text file containing the encrypted p
 
 Here’s a list of all the command line switches and example configurations.
 
-``` txt
--Office
-```
-
-The folder containing the Office Deployment Tool (ODT).
-
-``` txt
--Config
-```
-
-The name of the configuration xml file for the Office ODT. It must be located in the same folder as the ODT.
-
-``` txt
--Days
-```
-
-The number of days that you wish to keep old update files for. If you do not configure this option, no old files will be removed.
-
-``` txt
--L
-```
-
-The path to output the log file to. The file name will be Office-Update.log.
-
-``` txt
--Subject
-```
-
-The email subject that the email should have. Encapulate with single or double quotes.
-
-``` txt
--SendTo
-```
-
-The e-mail address the log should be sent to.
-
-``` txt
--From
-```
-
-The from address the log should be sent from.
-
-``` txt
--Smtp
-```
-
-The DNS name or IP address of the SMTP server.
-
-``` txt
--User
-```
-
-The user account to connect to the SMTP server.
-
-``` txt
--Pwd
-```
-
-The password for the user account.
-
-``` txt
--UseSsl
-```
-
-Connect to the SMTP server using SSL.
+| Command Line Switch | Description | Example |
+| ------------------- | ----------- | ------- |
+| -Office | The folder containing the Office Deployment Tool (ODT). | \\\server\share\office-365-x64\setup.exe |
+| -Config | The name of the configuration xml file for the Office ODT. It must be located in the same folder as the ODT. | config-365-x64.xml |
+| -Days | The number of days that you wish to keep old update files for. If you do not configure this option, no old files will be removed. | 30 |
+| -NoBanner | Use this option to hide the ASCII art title in the console. | N/A |
+| -L | The path to output the log file to. The file name will be Office-Update_YYYY-MM-dd_HH-mm-ss.log. Do not add a trailing \ backslash. | C:\scripts\logs |
+| -Subject | The subject line for the e-mail log. Encapsulate with single or double quotes. If no subject is specified, the default of "Office Update Utility Log" will be used. | 'Server: Notification' |
+| -SendTo | The e-mail address the log should be sent to. | me@contoso.com |
+| -From | The e-mail address the log should be sent from. | OffUpdate@contoso.com |
+| -Smtp | The DNS name or IP address of the SMTP server. | smtp.live.com OR smtp.office365.com |
+| -User | The user account to authenticate to the SMTP server. | example@contoso.com |
+| -Pwd | The txt file containing the encrypted password for SMTP authentication. | C:\scripts\ps-script-pwd.txt |
+| -UseSsl | Configures the utility to connect to the SMTP server using SSL. | N/A |
 
 ### Example
 
 ``` txt
-Office-Update.ps1 -Office C:\officesrc -Config config.xml -Days 60 -L C:\scripts\logs -Subject 'Server: Office Update' -SendTo me@contoso.com -From Office-Update@contoso.com -Smtp exch01.contoso.com -User me@contoso.com -Pwd P@ssw0rd -UseSsl
+Office-Update.ps1 -Office \\Apps01\Software\Office365 -Config config-365-x64.xml -Days 30 -L C:\scripts\logs -Subject 'Server: Office Update' -SendTo me@contoso.com -From OffUpdate@contoso.com -Smtp exch01.contoso.com -User me@contoso.com -Pwd P@ssw0rd -UseSsl
 ```
 
-The above command will run the script, download the Office files to C:\officesrc\Office. It will use a configuration file called config.xml in the C:\officesrc folder. Any update files older than 60 days will be removed. If the download is successful a log file is generated and it can be e-mailed with a custom subject line as a notification that a download occurred.
+The above command will download any Office updates for the version and channel configured in config-365-x64.xml to the Office files directory \\Apps01\Software\Office365. Any update files older than 30 days will be removed. If the download is successful the log file will be output to C:\scripts\logs and e-mailed with a custom subject line.
