93ca02814372fa09eaac2ab70a1c4184fd69d570
Digressive <40495197+Digressive@users.noreply.github.com>
2022-05-30 17:09:45 +0100

    Update Office-Update.ps1

--- Office-Update.ps1
+++ Office-Update.ps1
@@ -259,7 +259,6 @@ else {
     ##
     ## Display the current config and log if configured.
     ##
-
     Write-Log -Type Conf -Evt "************ Running with the following config *************."
     Write-Log -Type Conf -Evt "Utility Version:.......22.05.25"
     Write-Log -Type Conf -Evt "Hostname:..............$Env:ComputerName."
